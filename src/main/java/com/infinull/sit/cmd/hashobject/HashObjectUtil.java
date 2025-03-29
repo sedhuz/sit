9c436c06f0adb5b6ad7f120fbe373b4fa033fbf2
sedhu.b <sedhu.b@zohocorp.com>
2025-03-29 16:46:30 +0530

    Optimized HashObjectUtil and fixed vulnerability
    - Path Injection vulnerability was fixed

--- src/main/java/com/infinull/sit/cmd/hashobject/HashObjectUtil.java
+++ src/main/java/com/infinull/sit/cmd/hashobject/HashObjectUtil.java
@@ -8,20 +8,23 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.zip.DeflaterOutputStream;
 
 public class HashObjectUtil {
-    private final static byte[] BLOB_BYTES = "blob".getBytes(StandardCharsets.UTF_8);
-    private final static byte[] SPACE_BYTES = " ".getBytes(StandardCharsets.UTF_8);
-    private final static byte[] NULL_BYTES = "\0".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] BLOB_BYTES = "blob".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] SPACE_BYTES = " ".getBytes(StandardCharsets.UTF_8);
+    private static final byte[] NULL_BYTES = "\0".getBytes(StandardCharsets.UTF_8);
+    private static final String BASE_DIR = System.getProperty("user.dir");
 
     public static String getSha(String filePath, boolean canWriteFile) {
         File file = getFile(filePath);
         try {
             byte[] fileContent = Files.readAllBytes(file.toPath());
-            byte[] fileContentLength = (String.valueOf(fileContent.length)).getBytes(StandardCharsets.UTF_8);
+            byte[] fileContentLength = String.valueOf(fileContent.length).getBytes(StandardCharsets.UTF_8);
             byte[] objectData = concatenate(BLOB_BYTES, SPACE_BYTES, fileContentLength, NULL_BYTES, fileContent);
             String sha = computeSha1(objectData);
             if (canWriteFile) {
@@ -34,9 +37,11 @@ public class HashObjectUtil {
     }
 
     public static File getFile(String absolutePath) {
-        String cwd = System.getProperty("user.dir");
-        String filePath = cwd + "/" + absolutePath;
-        File file = new File(filePath);
+        Path path = Paths.get(BASE_DIR, absolutePath).normalize();
+        if (!path.startsWith(BASE_DIR)) {
+            throw new SitException(1, "error.file.exist", absolutePath);
+        }
+        File file = path.toFile();
         if (!file.exists()) {
             throw new SitException(1, "error.file.exist", absolutePath);
         }
@@ -44,14 +49,13 @@ public class HashObjectUtil {
     }
 
     private static void writeObjectToGit(String sha, byte[] objectData) {
-        try {
-            String objectPath = ".git/objects/" + sha.substring(0, 2) + "/" + sha.substring(2);
-            File objectFile = new File(objectPath);
-            objectFile.getParentFile().mkdirs();
-            try (OutputStream outputStream = new FileOutputStream(objectFile); DeflaterOutputStream deflater = new DeflaterOutputStream(outputStream)) {
-                deflater.write(objectData);
-                deflater.finish(); // Ensure all data is flushed
-            }
+        String objectPath = ".git/objects/" + sha.substring(0, 2) + "/" + sha.substring(2);
+        File objectFile = new File(objectPath);
+        objectFile.getParentFile().mkdirs();
+        try (OutputStream outputStream = new FileOutputStream(objectFile);
+             DeflaterOutputStream deflater = new DeflaterOutputStream(outputStream)) {
+            deflater.write(objectData);
+            deflater.finish();
         } catch (IOException e) {
             throw new SitException(1, "error.file.write", sha);
         }
